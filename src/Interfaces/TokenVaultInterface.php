e7f30ae89589cf52fe89068c27c9b201e7d4c9d1
timothy <timothy.decort@polaris-dc.com>
2021-10-12 22:20:32 +0200

    Update TokenVaultInterface

--- src/Interfaces/TokenVaultInterface.php
+++ src/Interfaces/TokenVaultInterface.php
@@ -6,11 +6,13 @@ namespace PolarisDC\ExactOnline\BaseClient\Interfaces;
 
 interface TokenVaultInterface
 {
-    public function makeToken(?string $accesToken, ?string $refreshToken, int $expiresAt): AccessTokenInterface;
+    public function makeToken(?string $accessToken, ?string $refreshToken, int $expiresAt): AccessTokenInterface;
 
     public function store(AccessTokenInterface $accessToken): void;
 
     public function retrieve(): AccessTokenInterface;
+
+    public function remove(string $accessToken): void;
 
     public function clear(): void;
 }
