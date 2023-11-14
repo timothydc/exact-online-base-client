7b6f630c65c7772c6a01e4a90ae63e9d9dddbf59
timothy <Timothy.DeCort@polaris-dc.com>
2023-11-14 17:27:33 +0100

    Add fallback when SemaphoreStore extension is not loaded

--- src/Traits/Lockable.php
+++ src/Traits/Lockable.php
@@ -6,6 +6,7 @@ namespace TimothyDC\ExactOnline\BaseClient\Traits;
 
 use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Lock\LockInterface as Lock;
+use Symfony\Component\Lock\Store\FlockStore;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 
 trait Lockable
@@ -26,13 +27,18 @@ trait Lockable
 
     /**
      * Opted to use the low level atomic locking (SemaphoreStore variant) of Symfony.
-     *
+     * Use the normal file locking as fallback
+     * 
      * @see https://symfony.com/doc/4.4/components/lock.html#usage
      */
     protected function getLock(string $lockKey = '', int $lockTimeout = 0): Lock
     {
         if (! isset($this->lock)) {
-            $this->lock = (new LockFactory(new SemaphoreStore()))
+            $lockStore = extension_loaded('sysvsem')
+                ? new SemaphoreStore()
+                : new FlockStore();
+
+            $this->lock = (new LockFactory($lockStore))
                 ->createLock($lockKey ?: $this->lockKey, $lockTimeout ?: $this->lockTimeout);
         }
 
